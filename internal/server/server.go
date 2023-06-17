d9562bd74cfc60c687cdb98b172da8d5d98e280f
begenov <begenovorazali@icloud.com>
2023-06-17 10:23:50 +0600

    init server

---
+++ internal/server/server.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"admin/internal/config"
+	"context"
+	"net/http"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func NewServer(cfg *config.Config, handler http.Handler) *Server {
+	return &Server{
+		httpServer: &http.Server{
+			Addr:           ":" + cfg.Server.Port,
+			Handler:        handler,
+			ReadTimeout:    cfg.Server.ReadTimeout,
+			WriteTimeout:   cfg.Server.WriteTimeout,
+			MaxHeaderBytes: cfg.Server.MaxHeaderMegabytes << 20,
+		},
+	}
+}
+
+func (s *Server) Run() error {
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
