2184249d6c2296be97089d14eb0a1e52c7f045e0
begenov <begenovorazali@icloud.com>
2023-06-17 10:25:52 +0600

    init pkg

---
+++ pkg/kafka/consumer.go
@@ -0,0 +1,69 @@
+package kafka
+
+import (
+	"log"
+
+	"github.com/Shopify/sarama"
+)
+
+type Consumer struct {
+	Consumer sarama.Consumer
+	done     chan struct{}
+}
+
+func NewConsumer(brokers []string) (*Consumer, error) {
+	config := sarama.NewConfig()
+
+	config.Consumer.IsolationLevel = sarama.ReadCommitted
+	config.Consumer.Offsets.AutoCommit.Enable = false
+	config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRoundRobin
+	consumer, err := sarama.NewConsumer(brokers, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Consumer{
+		Consumer: consumer,
+		done:     make(chan struct{}),
+	}, nil
+}
+
+func (c *Consumer) ConsumeMessages(topic string, handler func(message string)) error {
+	partitions, err := c.Consumer.Partitions(topic)
+	if err != nil {
+		log.Println("Failed to retrieve partitions:", err)
+		return err
+	}
+
+	for _, partition := range partitions {
+		pc, err := c.Consumer.ConsumePartition(topic, partition, sarama.OffsetNewest)
+		if err != nil {
+			log.Println("Failed to start consumer for partition", partition, ":", err)
+			return err
+		}
+		go c.consumePartition(pc, handler)
+	}
+
+	return nil
+}
+
+func (c *Consumer) consumePartition(pc sarama.PartitionConsumer, handler func(message string)) {
+	defer pc.Close()
+
+	for message := range pc.Messages() {
+		handler(string(message.Value))
+	}
+}
+
+func (c *Consumer) Stop() {
+	close(c.done)
+}
+
+func (c *Consumer) Close() error {
+	c.Stop()
+
+	if c.Consumer != nil {
+		return c.Consumer.Close()
+	}
+	return nil
+}
---
+++ pkg/kafka/produser.go
@@ -0,0 +1,61 @@
+package kafka
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Shopify/sarama"
+)
+
+type Producer struct {
+	producer sarama.AsyncProducer
+}
+
+func NewProducer(brokers []string) (*Producer, error) {
+	config := sarama.NewConfig()
+
+	config.Producer.RequiredAcks = sarama.WaitForLocal
+	config.Producer.Compression = sarama.CompressionSnappy
+	config.Producer.Flush.Frequency = 500 * time.Millisecond
+	fmt.Println(brokers)
+	producer, err := sarama.NewAsyncProducer(brokers, config)
+	if err != nil {
+		log.Fatalln("Failed to start Sarama producer:", err)
+	}
+
+	go func() {
+		for err := range producer.Errors() {
+			log.Println("Failed to write access log entry:", err)
+		}
+	}()
+
+	return &Producer{
+		producer: producer,
+	}, nil
+}
+
+func (p *Producer) SendMessage(topic string, data interface{}) error {
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatalln("error json marsal", err)
+		return err
+	}
+
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(jsonData),
+	}
+
+	p.producer.Input() <- msg
+	return nil
+}
+
+func (p *Producer) Close() error {
+	if p.producer != nil {
+		return p.producer.Close()
+	}
+	return nil
+}
